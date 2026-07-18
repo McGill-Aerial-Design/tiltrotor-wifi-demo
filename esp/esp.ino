fcce40a16a562109ac85ce7b8428f07b42fc4491
Maximilian Newman Loussouarn <maximilian.newman@icloud.com>
2026-07-18 20:12:16 +0200

    finished web interface
    still no control of the servos yet (will be done on an arduino connected to the esp by uart)
    
    Completely untested so far :(
    can't test anything until I'm back in montreal.

--- esp/esp.ino
+++ esp/esp.ino
@@ -11,12 +11,14 @@ int pitch = 0; // -2, -1, 0, 1, 2
 int yaw = 0;   // -2, -1, 0, 1, 2
 bool horizontalMode = false;
 
+const char* redirectToHome = "<br><p>You will be automatically redirected to the main page in 5 seconds.</p>\n<meta http-equiv = 'refresh' content = '2; url = /' />\n";
+const char* niceTryBro = "<h1>Nice Try Bro</h1>\n<p>Did you really think I would let you break stuff by injecting arbitrary values everywhere?</p>\n<p>Needless to say, your request was blocked</p>\n";
 
 void setup() {
   Serial.begin(115200);
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP("Secret Santa", "azertyui");
+  WiFi.softAP("MAD tiltrotor demo");
   dnsServer.start(53, "*", apIP);
 }
 
@@ -44,7 +46,7 @@ void loop() {
     
 
 
-    if (req.startsWith("GET /robots.txt") or req.startsWith("GET /boaform/admin") or req.startsWith("GET /sitemap.xml") or req.startsWith("GET /.well-known/security.txt") or req.startsWith("GET /.vscode/sftp.json") or req.startsWith("GET /client/get_targets") or req.startsWith("GET /showLogin.cc") or req.startsWith("GET http://") or req.startsWith("GET /HNAP") or req.startsWith("GET /config/getuser?") or req.startsWith("GET /.git/config") or req.startsWith("GET jira/jira-webapp-dist/pom.properties")) {
+    if (req.startsWith("POST") or req.startsWith("GET /robots.txt") or req.startsWith("GET /boaform/admin") or req.startsWith("GET /sitemap.xml") or req.startsWith("GET /.well-known/security.txt") or req.startsWith("GET /.vscode/sftp.json") or req.startsWith("GET /client/get_targets") or req.startsWith("GET /showLogin.cc") or req.startsWith("GET http://") or req.startsWith("GET /HNAP") or req.startsWith("GET /config/getuser?") or req.startsWith("GET /.git/config") or req.startsWith("GET jira/jira-webapp-dist/pom.properties")) {
       client.print("HTTP/1.1 404 Not Found\n");
       client.print("Connection: close\n\n");
     }
@@ -63,29 +65,109 @@ void loop() {
       client.print("<title>MAD Tilt-Rotor Control</title>\n");
       client.print("</head> <body>\n");
 
-      client.print("<h1>McGill Aerial Design</h1>\n");
-      client.print("<p>This page allows you to send commands to the tilt-rotor drone to demonstrate how the control surfaces work.</p>\n");
+      if (req.startsWith("GET /transition")){
+        client.print("<p>Transitioning...</p>");
+        client.print(redirectToHome);
+        horizontalMode = !horizontalMode;
+      }
 
-      client.print("<p><b>Current state:</b></p>\n");
+      else if (req.startsWith("GET /flap")){
+        int newVal = req.substring(9).toInt();
+        if (newVal >= 0 and newVal <= 2) {
+          client.print("<p>Flaps set to " + String(newVal) + "</p>");
+          flap = newVal;
+        }
+        else {client.print(niceTryBro);}
+        client.print(redirectToHome);
+      }
 
-      if (horizontalMode) {client.print("<p>transition mode: horizontal flight</p\n");}
-      else {client.print("<p>transition mode: vertical flight</p\n");}
+      else if (req.startsWith("GET /pitch")){
+        int newVal = req.substring(10).toInt();
+        if (newVal >= -2 and newVal <= 2) {
+          client.print("<p>Pitch set to " + String(newVal) + "</p>");
+          pitch = newVal;
+        }
+        else {client.print(niceTryBro);}
+        client.print(redirectToHome);
+      }
 
-      client.print("<p>flaps: " + String(flap) + " / 2</p>\n");
+      else if (req.startsWith("GET /roll")){
+        int newVal = req.substring(9).toInt();
+        if (newVal >= -2 and newVal <= 2) {
+          client.print("<p>Roll set to " + String(newVal) + "</p>");
+          roll = newVal;
+        }
+        else {client.print(niceTryBro);}
+        client.print(redirectToHome);
+      }
 
-      String s = "neutral";
-      if (pitch == -2) {s = "strong down";}
-      if (pitch == -1) {s = "weak down";}
-      if (pitch == 1) {s = "weak up";}
-      if (pitch == 2) {s = "strong up";}
-      client.print("<p>pitch: " + s + "</p>\n");
+      else if (req.startsWith("GET /yaw")){
+        int newVal = req.substring(9).toInt();
+        if (newVal >= -2 and newVal <= 2) {
+          client.print("<p>Yaw set to " + String(newVal) + "</p>");
+          yaw = newVal;
+        }
+        else {client.print(niceTryBro);}
+        client.print(redirectToHome);
+      }
 
-      s = "neutral";
-      if (roll == -2) {s = "strong left";}
-      if (roll == -1) {s = "weak left";}
-      if (roll == 1) {s = "weak right";}
-      if (roll == 2) {s = "strong right";}
-      client.print("<p>roll: " + s + "</p>\n");
+
+
+      else {
+        client.print("<h1>McGill Aerial Design</h1>\n");
+        client.print("<p>This page allows you to send commands to the tilt-rotor drone to demonstrate how the control surfaces work.</p>\n");
+
+        client.print("<p>Current mode: ");
+
+        if (horizontalMode) {client.print("horizontal");}
+        else {client.print("vertical");}
+        client.print(" flight   <a href='/transition'><button>Transition</button></a> </p>\n");
+
+        client.print("<h3>Flaps</h3>\n");
+        client.print("<p>currently: " + String(flap) + " / 2</p>\n");
+        client.print("<p> <a href='/flap0'><button>0</button></a>");
+        client.print("<a href='/flap1'><button>1</button></a>");
+        client.print("<a href='/flap2'><button>2</button></a> </p>\n");
+
+        client.print("<h3>Pitch</h3>\n");
+        String s = "neutral";
+        if (pitch == -2) {s = "strong down";}
+        if (pitch == -1) {s = "weak down";}
+        if (pitch == 1) {s = "weak up";}
+        if (pitch == 2) {s = "strong up";}
+        client.print("<p>currently: " + s + "</p>\n");
+        client.print("<p> <a href='/pitch-2'><button>strong down</button></a>");
+        client.print("<a href='/pitch-1'><button>1</weak down></a>");
+        client.print("<a href='/pitch0'><button>1</neutral></a>");
+        client.print("<a href='/pitch+1'><button>1</weak up></a>");
+        client.print("<a href='/pitch+2'><button>2</strong up></a> </p>\n");
+
+        client.print("<h3>Roll</h3>\n");
+        s = "neutral";
+        if (roll == -2) {s = "strong left";}
+        if (roll == -1) {s = "weak left";}
+        if (roll == 1) {s = "weak right";}
+        if (roll == 2) {s = "strong right";}
+        client.print("<p>currently: " + s + "</p>\n");
+        client.print("<p> <a href='/roll-2'><button>strong left</button></a>");
+        client.print("<a href='/roll-1'><button>1</weak left></a>");
+        client.print("<a href='/roll0'><button>1</neutral></a>");
+        client.print("<a href='/roll+1'><button>1</weak right></a>");
+        client.print("<a href='/roll+2'><button>2</strong right></a> </p>\n");
+
+        client.print("<h3>Yaw</h3>\n");
+        s = "neutral";
+        if (yaw == -2) {s = "strong left";}
+        if (yaw == -1) {s = "weak left";}
+        if (yaw == 1) {s = "weak right";}
+        if (yaw == 2) {s = "strong right";}
+        client.print("<p>currently: " + s + "</p>\n");
+        client.print("<p> <a href='/yaw-2'><button>strong left</button></a>");
+        client.print("<a href='/yaw-1'><button>1</weak left></a>");
+        client.print("<a href='/yaw0'><button>1</neutral></a>");
+        client.print("<a href='/yaw+1'><button>1</weak right></a>");
+        client.print("<a href='/yaw+2'><button>2</strong right></a> </p>\n");
+      }
 
 
 
