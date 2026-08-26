8997fe743d3d3ccc3696874c6bdd03d211e38080
Maximilian Newman Loussouarn <maximilian.newman@icloud.com>
2026-08-26 14:41:45 -0400

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -9,10 +9,10 @@ Servo servoRudder;
 Servo servoTiltL;
 Servo servoTiltR;
 
-int flapState = 0;  // 0, 1, 2
-int rollState = 0;  // -2, -1, 0, 1, 2
-int pitchState = 0; // -2, -1, 0, 1, 2
-int yawState = 0;   // -2, -1, 0, 1, 2
+byte flapState = 0;  // 0, 1, 2
+byte rollState = 0;  // 0, 1, 2, 3, 4
+byte pitchState = 0; // 0, 1, 2, 3, 4
+byte yawState = 0;   // 0, 1, 2, 3, 4
 bool horizontalMode = false;
 bool targetHorizontalMode = false;
 
@@ -26,25 +26,25 @@ unsigned int currentRTiltPWM = 1500;
 unsigned int targetLTiltPWM = 1500;
 unsigned int targetRTiltPWM = 1500;
 
-const unsigned int aileronRPWMs[] = {1500, 1500, 1500, 1500, 1500}; // tbd
+const unsigned int aileronRPWMs[] = {1150, 1300, 1450, 1600, 1750};
 const unsigned int aileronLPWMs[] = {1700, 1550, 1400, 1275, 1150};
 const unsigned int flapRPWMs[] = {950, 1175, 1400};
-const unsigned int flapLPWMs[] = {1500, 1500, 1500}; // tbd
+const unsigned int flapLPWMs[] = {1950, 1750, 1550};
 const unsigned int elevatorPWMs[] = {1500, 1500, 1500, 1500, 1500}; // tbd
 const unsigned int rudderPWMs[] = {1500, 1500, 1500, 1500, 1500}; // tbd
 const unsigned int tiltRPWMs[] = {1500, 1500}; // [horizontal, vertical] tbd
-const unsigned int tiltLPWMs[] = {1500, 1500}; // tbd
-const unsigned int verticalFlightAuthority = 20;
+const unsigned int tiltLPWMs[] = {1000, 1700};
+const unsigned int verticalFlightAuthority = 100;
 
 void setup() {
-  servoFlapL.attach(0); // pins tbd
-  servoFlapR.attach(0);
-  servoAileronL.attach(0);
-  servoAileronR.attach(0);
-  servoElevator.attach(0);
-  servoRudder.attach(0);
-  servoTiltL.attach(0);
-  servoTiltR.attach(0);
+  servoFlapL.attach(7);
+  servoFlapR.attach(2);
+  servoAileronL.attach(5);
+  servoAileronR.attach(4);
+  servoElevator.attach(9);
+  servoRudder.attach(10);
+  servoTiltL.attach(6);
+  servoTiltR.attach(3);
 
   Serial.begin(115200);
 }
@@ -58,15 +58,21 @@ void loop() {
       pitchState = Serial.read();
       rollState = Serial.read();
       yawState = Serial.read();
+
+      if (flapState > 2) {flapState = 0;}
+      if (rollState > 4) {rollState = 2;}
+      if (pitchState > 4) {pitchState = 2;}
+      if (yawState > 4) {yawState = 2;}
+      //Serial.println(rollState);
     }
   }
 
   if (horizontalMode == targetHorizontalMode) {
     if (horizontalMode) {
-      servoAileronL.writeMicroseconds(aileronLPWMs[rollState+2]);
-      servoAileronR.writeMicroseconds(aileronRPWMs[rollState+2]);
-      servoElevator.writeMicroseconds(elevatorPWMs[pitchState+2]);
-      servoRudder.writeMicroseconds(rudderPWMs[yawState+2]);
+      servoAileronL.writeMicroseconds(aileronLPWMs[rollState]);
+      servoAileronR.writeMicroseconds(aileronRPWMs[rollState]);
+      servoElevator.writeMicroseconds(elevatorPWMs[pitchState]);
+      servoRudder.writeMicroseconds(rudderPWMs[yawState]);
       targetRTiltPWM = tiltRPWMs[0];
       targetLTiltPWM = tiltLPWMs[0];
     }
@@ -88,12 +94,12 @@ void loop() {
   else if (targetHorizontalMode) {
     targetRFlapPWM = flapRPWMs[2];
     targetLFlapPWM = flapLPWMs[2];
-    currentRFlapPWM = flapRPWMs[2];
-    currentLFlapPWM = flapLPWMs[2];
-    targetRTiltPWM = tiltRPWMs[0];
-    targetLTiltPWM = tiltLPWMs[0];
-    if (currentRTiltPWM == tiltRPWMs[0] and currentLTiltPWM == tiltLPWMs[0]) {
-      horizontalMode = true;
+    if (currentRFlapPWM == flapRPWMs[2] and currentLFlapPWM == flapLPWMs[2]) { // wait for flaps to extend
+      targetRTiltPWM = tiltRPWMs[0];
+      targetLTiltPWM = tiltLPWMs[0];
+      if (currentRTiltPWM == tiltRPWMs[0] and currentLTiltPWM == tiltLPWMs[0]) {
+        horizontalMode = true;
+      }
     }
   }
   else {
